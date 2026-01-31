4952aec4336b2b3b7bd271bffc111473a86914f1
linic <linic@hotmail.ca>
2026-01-31 14:14:59 -0500

    updated rust-i586.tcz, added tags.db.gz in 17.x/x86/tcz

--- update-dbs.sh
+++ update-dbs.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-USAGE="Usage: ./update-dbs.sh"
-if [ ! $# -eq 0 ]; then
-  echo "$USAGE"
-  exit 1
-fi
-# Subject to change.
-# What I consider databases which are updated by this script:
-# - 16.x/x86/tcz/tags.db.gz
-# - 16.x/x86/tcz/info.lst
-# - 16.x/x86/tcz.html
-cd 16.x/x86/tcz/
-ls *.tcz > info.lst
-cp info.lst ../tcz.html
-# Updating tags.db.gz
-gunzip tags.db.gz
 
-: > tags.db.tmp   # temporary output
+###################################################################
+# Copyright (C) 2026  linic@hotmail.ca Subject to GPL-3.0 license.#
+# https://git@github.com:linic/tcz.facedebouc.sbs.git             #
+###################################################################
 
-for f in *.tcz; do
+TC17="17.x/x86/tcz"
+TC16="16.x/x86/tcz"
+
+update_dbs()
+{
+  # Subject to change.
+  # What I consider databases which are updated by this script:
+  # - 16.x/x86/tcz/tags.db.gz
+  # - 16.x/x86/tcz/info.lst
+  # - 16.x/x86/tcz.html
+  cd "$1"
+  ls *.tcz > info.lst
+  cp info.lst ../tcz.html
+  # Updating tags.db.gz
+  gunzip tags.db.gz
+
+  : > tags.db.tmp   # temporary output
+
+  for f in *.tcz; do
     # skip if already present in tags.db
     if [ -f tags.db ] && grep -q "^$f\\b" tags.db; then
         continue
@@ -31,10 +37,41 @@ for f in *.tcz; do
     else
         printf '%s\n' "$f" >> tags.db.tmp
     fi
-done
+  done
 
-# append the diff in .tmp
-cat tags.db.tmp >> tags.db
-rm -f tags.db.tmp
-gzip tags.db
-cd -
+  # append the diff in .tmp
+  cat tags.db.tmp >> tags.db
+  rm -f tags.db.tmp
+  gzip tags.db
+  cd $OLDPWD
+}
+
+usage()
+{
+  echo "Usage: ./update-dbs.sh [path]"
+  echo "example ./update-dbs.sh 16.x/x86/tcz"
+  return 2
+}
+
+main()
+{
+  if [ ! $# -eq 1 ]; then
+    usage
+    exit $?
+  fi
+  case "$1" in
+    "$TC17")
+      update_dbs "$@"
+      exit $?
+      ;;
+    "$TC16")
+      update_dbs "$@"
+      exit $?
+      ;;
+    *)
+      usage
+      ;;
+  esac
+}
+
+main "$@"
