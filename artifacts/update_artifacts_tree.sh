d96032c2e3991ba8a253fdb2cb479dd8a79b8bfb
linic <linic@hotmail.ca>
2026-02-14 05:54:47 -0500

    navigate.sh

--- artifacts/update_artifacts_tree.sh
+++ artifacts/update_artifacts_tree.sh
@@ -1,2 +1,93 @@
 #!/bin/sh
-tree . > artifacts.tree.html
+
+##############################################################
+# Copyright (C) 2026 linic@hotmail.ca under GPL-3.0 license. #
+# https://github.com/linic/tcz.facedebouc.sbs                #
+##############################################################
+
+HTML_PAGE="artifacts.html"
+LIST_FILE="artifacts.lst"
+TREE_PREFIX="├─"
+
+usage()
+{
+  echo "./update_artifacts_tree.sh [files]"
+}
+
+generate_all_files()
+{
+  tree --noreport -H . >> $HTML_PAGE
+  tree --noreport . | sed 's/[^a-zA-Z0-9.-_]//g' > $LIST_FILE
+}
+
+generate_files()
+{
+  rm "../$LIST_FILE"
+  touch "../$LIST_FILE"
+  rm $HTML_PAGE
+  touch $HTML_PAGE
+  echo "<!DOCTYPE html>" >> $HTML_PAGE
+  echo "<html><head><title>$HTML_PAGE</title><meta charset="UTF-8"></head><body><p><code><pre>" >> $HTML_PAGE
+  walk "." "." $TREE_PREFIX "."
+  result=$?
+  echo "</pre></code></p></body></html>" >> $HTML_PAGE
+  return $result
+}
+
+walk()
+{
+  NEXT_DIR_NAME=$1
+  RELATIVE_PATH=$2
+  TREE_LEVEL=$3
+  FILES_DIR=$4
+  cd $NEXT_DIR_NAME
+  ls | while read line; do
+    if [ -d "$line" ]; then
+      walk "$line" "$RELATIVE_PATH/$line" "$TREE_LEVEL$TREE_PREFIX" "$FILES_DIR/.."
+      if [ $? != 0 ]; then
+        return "$?"
+      fi
+    elif [ -f "$line" ]; then
+      echo "artifacts/$RELATIVE_PATH/$line" | sed "s|\./||" >> "../$FILES_DIR/$LIST_FILE"
+      echo "$TREE_LEVEL <a href=\"$RELATIVE_PATH/$line\">$line</a>" >> "$FILES_DIR/$HTML_PAGE"
+    else
+      echo "Ignoring $line"
+    fi
+  done
+  return 0
+}
+
+add_links()
+{
+  while read line; do
+    name=`echo "$line" | cut -d' ' -f2`
+    path=`find -name "$name" -printf "%P\n" | head -n 1`
+    if [ -f "$path" ]; then
+     href="<a href=\"./$path\">$name</a>"
+     sed -i "s|$name|$href|" $HTML_PAGE
+     echo "tried to replace $name with $href"
+   else
+     echo "$path does not exist"
+    fi
+  done < $LIST_FILE
+}
+
+main()
+{
+  case "$1" in
+    links)
+      echo "running an old iteration, this won't do what you want probably"
+      echo "it worked on a tree.txt file"
+      add_links
+      ;;
+    files)
+      generate_files
+      ;;
+    *)
+      usage
+      ;;
+  esac
+  exit "$?"
+}
+
+main "$@"
---
+++ navigate.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+
+##############################################################
+# Copyright (C) 2026 linic@hotmail.ca under GPL-3.0 license. #
+# https://github.com/linic/tcz.facedebouc.sbs                #
+##############################################################
+
+usage()
+{
+  echo "./navigate.sh [list | download [artifact_name]] [url]"
+  echo "example: ./navigate.sh list https://tcz.facedebouc.sbs"
+}
+
+get_artifacts_lst()
+{
+  ARTIFACTS_LST=`wget -qO- "$BASE_URL/artifacts.lst"`
+  return $?
+}
+
+get_download_info()
+{
+  ORGANIZATION=`wget -qO- "$BASE_URL/organization.txt"`
+  #ORGANIZATION=`wget -qO- "$BASE_URL/16.x/x86/tcz/organization.txt"`
+  if [ $? != 0 ] || [ -z "$ORGANIZATION" ]; then
+    return 20
+  fi
+  REPOSITORY=`wget -qO- "$BASE_URL/repository.txt"`
+  #REPOSITORY=`wget -qO- "$BASE_URL/16.x/x86/tcz/repository.txt"`
+  if [ $? != 0 ] || [ -z "$REPOSITORY" ]; then
+    return 21
+  fi
+  URL=https://github.com/$ORGANIZATION/$REPOSITORY.git/info/lfs/objects/batch
+  H1="Accept: application/vnd.git-lfs+json"
+  H2="Content-type: application/json"
+  BODY="{\"operation\": \"download\", \"transfers\": [\"basic\"], \"ref\": {\"name\": \"refs/heads/main\"}, \"objects\": [{\"oid\": \"$SHA\", \"size\": $SIZE}], \"hash_algo\": \"sha256\"}"
+  DOWNLOAD_INFO=`curl -s -X POST -H "$H1" -H "$H2" -d "$BODY" $URL`
+  if [ $? != 0 ] || [ -z "$REPOSITORY" ]; then
+    return 22
+  fi
+}
+
+regular_download()
+{
+  wget -c -O "$ARTIFACT_NAME" "$BASE_URL/$ARTIFACT_RELATIVE_PATH"
+  if [ $? != 0 ]; then
+    echo "Failed to download $BASE_URL/$ARTIFACT_RELATIVE_PATH."
+    return 50
+  else
+    echo "Downloaded $BASE_URL/$ARTIFACT_RELATIVE_PATH directly."
+    echo "Maybe it is not in the github lfs."
+    echo "When a file is not in the github lfs, download it directly."
+    echo "Please inspect the file $ARTIFACT_NAME."
+    return 0
+  fi
+}
+
+download()
+{
+  get_artifacts_lst
+  ARTIFACT_RELATIVE_PATH=`echo "$ARTIFACTS_LST" | grep --text "$ARTIFACT_NAME" | head -1`
+  if [ ! -z "$ARTIFACT_RELATIVE_PATH" ]; then
+    pointer=`wget -qO- "$BASE_URL/$ARTIFACT_RELATIVE_PATH"`
+    if [ $? != 0 ] || [ -z "$pointer" ]; then
+      echo "Failed to get pointer: $pointer"
+      regular_download
+      return 10
+    fi
+    SHA=`echo "$pointer" | grep -o 'sha256:[^"]*' | cut -d':' -f2 | head -1`
+    if [ $? != 0 ] || [ -z "$SHA" ]; then
+      echo "Failed to extract SHA: $SHA"
+      regular_download
+      return 11
+    fi
+    SIZE=`echo "$pointer" | grep -o 'size [^"]*' | cut -d' ' -f2 | head -1`
+    if [ $? != 0 ] || [ -z "$SIZE"  ]; then
+      echo "Failed to extract SIZE: $SIZE"
+      regular_download
+      return 12
+    fi
+    get_download_info
+    HREF=`echo "$DOWNLOAD_INFO" | grep -o '"href":.*"[^"]*"' | cut -d'"' -f4 | head -1`
+    ARTIFACT_NAME=`echo "$ARTIFACT_RELATIVE_PATH" | rev | cut -d'/' -f1 | rev`
+    wget -c -O "$ARTIFACT_NAME" "$HREF"
+    if [ $? != 0 ]; then
+      regular_download
+      return 13
+    fi
+    return $?
+  else
+    echo "$ARTIFACT_NAME not found in $BASE_URL/artifacts.lst"
+    return 1
+  fi
+}
+
+main()
+{
+  case "$1" in
+    list)
+      BASE_URL=$2
+      if [ -z "$BASE_URL" ]; then
+        usage
+        exit 3
+      fi
+      get_artifacts_lst
+      result=$?
+      echo "$ARTIFACTS_LST"
+      exit $result
+      ;;
+    download)
+      BASE_URL=$3
+      if [ -z "$BASE_URL" ]; then
+        usage
+        exit 4
+      fi
+      ARTIFACT_NAME=$2
+      if [ -z "$ARTIFACT_NAME" ]; then
+        usage
+        exit 5
+      fi
+      download
+      exit $?
+      ;;
+    *)
+      usage
+      exit $?
+      ;;
+  esac
+}
+
+main "$@"
